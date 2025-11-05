f9219cd7d529b3d5e706d82e9a5943d918a5eec8
reyyynad <s202276760@kfupm.edu.sa>
2025-11-05 10:37:14 +0300

    To do 3 Done

--- calculator.js
+++ calculator.js
@@ -175,10 +175,41 @@ After completing all TODOs, test your calculator:
 
 */
 
+// todo 1
 import { add, subtract, multiply, divide } from "./utils/operations.js";
 import { parseNumbers, isValidOperation } from "./utils/parser.js";
 import _ from "lodash";
 
-
+// todo 2
 const operation = process.argv[2];
 const numbers = process.argv.slice(3);
+
+// todo 3
+if (!isValidOperation(operation)) {
+  console.log("Invalid operation. Use: add, subtract, multiply, or divide");
+  return;
+}
+
+const nums = parseNumbers(numbers);
+let result;
+
+switch (operation) {
+  case "add":
+    result = add(nums);
+    break;
+  
+  case "subtract":
+    result = substract(nums);
+    break;
+
+  case "multiply":
+    result = multiply(nums);
+    break;
+
+  case "divide":
+    result = divide(nums);
+    break;
+}
+
+
+console.log(`Result: ${result}`);
